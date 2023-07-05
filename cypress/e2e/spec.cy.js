e696e9dabc3e5a878b4b40ea397c8e21e86b2076
TomaKrastev <tomakrastev@gmail.com>
2023-07-05 17:56:11 +0300

    2

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,12 @@
 describe('Test BX Onboarding', () => {
   it ('Opens the BX Site', () => {
-    cy.visit(`https://assets.apps.horizonint.cloud/#/overview`)
-  
+    cy.visit(`https://assets.apps.horizonint.cloud/#/login`);
+    cy.get('[data-cy="login-auth-login-button"]').click();
+    cy.origin(`https://login-qa.siemens.com`, () => {
+    cy.get(`input[name="email"]`).type(`toma.krastev@siemens.com`);
+    cy.get('.auth0-label-submit').click();
+    })
+    
+    
   })
 })
